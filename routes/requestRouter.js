874056abd1cad48073ec1a57b63c08b6689556ce
Asad Abbas <khanasad92332@gmail.com>
2024-04-03 23:10:23 -0800

    updated

--- routes/requestRouter.js
+++ routes/requestRouter.js
@@ -1,5 +1,15 @@
 const express = require("express");
-const { getProductRequest, productRequest, getRequestByMonth, getRequestCategoryCount,getAllProductRequest, getProductRequestByRequestId,updateRequestStatus } = require("../controllers/requestController");
+const { 
+    getProductRequest,
+    productRequest,
+    getRequestByMonth,
+    getRequestCategoryCount,
+    getAllProductRequest,
+    getProductRequestByRequestId,
+    updateRequestStatus,
+    getUserRequestById,
+    updateUserRequestByIds,
+    getAllUserRequestedproduct } = require("../controllers/requestController");
 const router = express.Router();
 
 router.route("/getProductRequest/:user").get(getProductRequest)
@@ -9,5 +19,7 @@ router.route("/getAllProductRequest").get(getAllProductRequest)
 router.route("/getRequestByMonth/:user").get(getRequestByMonth)
 router.route("/getRequestCategoryCount/:user").get(getRequestCategoryCount)
 router.route("/productRequest").post(productRequest);
+router.route("/getAllUserRequestedProduct").get(getAllUserRequestedproduct);
+router.route("/updateUserRequestedProductById/:request_id/:product_id").put(updateUserRequestByIds);
 
 module.exports = router;
